21d23ce5b5d3f3f446fce79b144e64dd68a6657c
Sushmita Rai <sushmita@fusemachines.com>
2022-07-14 18:18:52 +0545

    :art: Improve format of the code

--- api/main.py
+++ api/main.py
@@ -8,23 +8,25 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 
-app = FastAPI(title='Information Extraction', version='1.0',
-              description='Testing')
+app = FastAPI(title="Information Extraction", version="1.0", description="Testing")
 
 NLP = spacy.load("./models/output/model-best")
 
+
 class Item(BaseModel):
-    paragraph:  Union[str, None] = None\
+    paragraph: Union[str, None] = None
+
 
 # TODO: response model
 
 
-@app.get('/')
+@app.get("/")
 def read_home():
     """
-     Home endpoint which can be used to test the availability of the application.
-     """
-    return {'message': 'System is healthy'}
+    Home endpoint which can be used to test the availability of the application.
+    """
+    return {"message": "System is healthy"}
+
 
 @app.post("/information-extractor")
 def predict(item: Item):
@@ -32,9 +34,14 @@ def predict(item: Item):
     para = data_dict["paragraph"]
     doc = NLP(para)
     prof = []
-    [prof.append(entity.text.capitalize()) for entity in doc.ents if entity.text.capitalize() not in prof]
+    [
+        prof.append(entity.text.capitalize())
+        for entity in doc.ents
+        if entity.text.capitalize() not in prof
+    ]
     res = [singularize(plural) for plural in prof]
-    return {"profession":res}
+    return {"profession": res}
+
 
 @app.post("/spellcorrector")
 def correction(item: Item):
@@ -42,7 +49,8 @@ def correction(item: Item):
     para = data_dict["paragraph"]
     spell = Speller()
     correct = spell(para)
-    return {"corrected sentence":correct}
+    return {"corrected sentence": correct}
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
